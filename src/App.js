dd4b88569478d928bb05b29ae3516b2a3a94ee71
Brennan Predmore <brennnann@gmail.com>
2020-11-27 01:28:08 -0600

    react router init on app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import Home from '../src/components/Pages/Home/Home'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from '../src/components/Pages/Home/Home';
+import Lesson from '../src/components/Pages/Lesson/Lesson'
+import Quiz from '../src/components/Pages/Quiz/Quiz';
+import Results from '../src/components/Pages/Results/Results'
 
 function App() {
   return (
+    <Router>
     <div className='App'>
-      <Home />
+      <Route exact path='/' component={Home} />
+      <Route exact path='/Lesson' component={Lesson} />
+      <Route exact path='/Quiz' component={Quiz} />
+      <Route exact path='/Results' component={Results} />
     </div>
+  </Router>
   );
 }
 
