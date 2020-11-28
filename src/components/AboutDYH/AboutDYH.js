19e66fd7acf6655a2baa71d934cc7f66be400691
Brennan Predmore <brennnann@gmail.com>
2020-11-27 20:17:13 -0600

    init about us component

---
+++ src/components/AboutDYH/AboutDYH.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './style.css';
+
+const AboutDYH = () => {
+  return (
+    <div>
+      <div className='my-container'>
+        <p>
+          Do Your Homework is a five piece hardcore punk band from Austin,
+          Texas. The band was formed in 2017 by four original members: Brennan
+          Predmoré, Matt Boyd, Luke Barry, and David Robin. Later in 2018, they
+          would add Chris Lansford a new member. The final line up was Brennan
+          Predmoré on drums, Matt Boyd and David Robin on guitar, Chris Lansford
+          on bass and Luke Barry on vocals.
+          <br />
+          Throughout the course of the next three years the band would play many
+          shows and go on 2 different Texas tours. DYH put out their first album
+          “Harder Not Smarter” in 2018 which included 9 hard hitting tracks and
+          was distributed on over 200 platforms worldwide as well as hard copy
+          CD’s. Their 2nd full length album “If They Came To Hear Me Beg” is due
+          to be released in 2021 which is their greatest work yet.
+          <br />
+          In 2020 the band has been on hiatus due to covid and hopes to still
+          play shows when venues and live shows come back to fruition.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutDYH;
--- src/components/Pages/Lesson/Lesson.js
+++ src/components/Pages/Lesson/Lesson.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from '../../Header/Header';
 import Footer from '../../Footer/Footer';
 import StartQuiz from '../../Buttons/StartQuiz/StartQuiz';
+import AboutDYH from '../../AboutDYH/AboutDYH';
 
 
 const Lesson = () => {
   return (
     <div>
       <Header />
+      <AboutDYH />
       <StartQuiz />
       <Footer />
     </div>
