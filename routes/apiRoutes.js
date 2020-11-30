c1515dbbba72928f01f8c39d0beade3293f6f051
Brennan Predmore <brennnann@gmail.com>
2020-11-30 02:44:35 -0600

    apiRoutes change

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,8 +4,8 @@ module.exports = (app) => {
   app.get('/api/code_quiz_react_questions', (req, res) => {
     fs.readFile('db/questionBank.json', (err, data) => {
       if (err) throw err;
-      let sexUnemploymentData = JSON.parse(data);
-      res.json(sexUnemploymentData);
+      let questionBankData = JSON.parse(data);
+      res.json(questionBankData);
     });
   });
 };
