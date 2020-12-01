7b1da46f7e7696a87b4e9515f0e04e70ab8a6bbe
Brennan Predmore <brennnann@gmail.com>
2020-11-30 18:58:00 -0600

    update model schema

--- models/Questions.js
+++ models/Questions.js
@@ -2,10 +2,20 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const QuestionsSchema = new Schema({
-    title: String,
-    choices: [String],
-    answer: String,
-  
+  title: {
+    type: String,
+    required: true,
+  },
+  choices: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  answer: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = questions = mongoose.model('questions', QuestionsSchema);
