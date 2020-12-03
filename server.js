daf7e51b3d63e703327291706cbd8869db9381a5
BrennanPredmore <brennnann@gmail.com>
2020-12-03 14:24:44 -0600

    remove default.json // rename files

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const db = require('./config/connection');
+const db = require('./mongoDB/connection');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
