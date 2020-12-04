5e25a8e7cb32cc729de7392bbdc6f8da2f388279
BrennanPredmore <brennnann@gmail.com>
2020-12-04 14:17:20 -0600

    viewResults page form & style init

--- client/src/components/Pages/Results/Results.js
+++ client/src/components/Pages/Results/Results.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../../Header/Header';
 import Footer from '../../Footer/Footer';
+import ViewResults from '../../ViewResults/ViewResults'
 
 const Results = () => {
   return (
     <div>
       <Header />
+      <ViewResults />
       <Footer />
     </div>
   );
