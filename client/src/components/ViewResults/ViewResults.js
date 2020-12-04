6aae0433ff9d5f40e868991ff26ba0413f8bae3d
Brennan Predmore <brennanpredmore@brennans-air.lan>
2020-12-04 14:17:20 -0600

    viewResults page form & style init

---
+++ client/src/components/ViewResults/ViewResults.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+const ViewResults = () => {
+  return (
+    <div>
+      <div className='viewResultsDIV'>
+        <h1 className={'header'}>View Results</h1>
+        <h3 className={'score'}>You scored:</h3>
+      </div>
+    </div>
+  );
+};
+
+export default ViewResults;
