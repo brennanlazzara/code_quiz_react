293f1d8e62fb01df8d2fc25778def4bc22d34d58
shawn220a <shawn.42496@gmail.com>
2020-12-04 18:32:02 -0600

    Move questions to own component

--- client/src/components/Pages/Quiz/Question1/Question1.js
+++ client/src/components/Pages/Quiz/Question1/Question1.js
@@ -1,26 +1,25 @@
 import React from 'react';
 
-const Question1 = ({question1}) => {
-    console.log(question1[0])
+const Question1 = ({ question }) => {
   return (
     <div className={'questionDIV'}>
-      {/* <h1 className={'title'}>{question1[0].title}</h1>
+      <h1 className={'title'}>{question[0].title}</h1>
       <div className='topQuestions'>
         <button className={'button'}>
-          <h5>{question1[0].choices[0]}</h5>
+          <h5>{question[0].choices[0]}</h5>
         </button>
         <button className={'button'}>
-          <h5>{question1[0].choices[1]}</h5>
+          <h5>{question[0].choices[1]}</h5>
         </button>
       </div>
       <div className='bottomQuestions'>
         <button className={'button'}>
-          <h5>{question1[0].choices[2]}</h5>
+          <h5>{question[0].choices[2]}</h5>
         </button>
         <button className={'button'}>
-          <h5>{question1[0].choices[3]}</h5>
+          <h5>{question[0].choices[3]}</h5>
         </button>
-      </div> */}
+      </div>
     </div>
   );
 };
--- client/src/components/Pages/Quiz/Question2/Question2.js
+++ client/src/components/Pages/Quiz/Question2/Question2.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
-const Question2 = (props) => {
+const Question2 = ({ question }) => {
   return (
     <div className={'questionDIV'}>
-      <h1 className={'title'}>{props.title}</h1>
+      <h1 className={'title'}>{question[1].title}</h1>
       <div className='topQuestions'>
         <button className={'button'}>
-          <h5>{props.choices[0]}</h5>
+          <h5>{question[1].choices[0]}</h5>
         </button>
         <button className={'button'}>
-          <h5>{props.choices[1]}</h5>
+          <h5>{question[1].choices[1]}</h5>
         </button>
       </div>
       <div className='bottomQuestions'>
         <button className={'button'}>
-          <h5>{props.choices[2]}</h5>
+          <h5>{question[1].choices[2]}</h5>
         </button>
         <button className={'button'}>
-          <h5>{props.choices[3]}</h5>
+          <h5>{question[1].choices[3]}</h5>
         </button>
       </div>
     </div>
