e90cdff107fbded3755146146f215498464679ff
Brennan Predmore <brennanpredmore@brennans-air.lan>
2020-12-07 16:49:56 -0600

    disable buttons after clicked

--- client/src/components/Pages/Quiz/Question2/Question2.js
+++ client/src/components/Pages/Quiz/Question2/Question2.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Question2 = ({ question }) => {
+  const [btnDisabled, setbtnDisabled] = useState(false);
+
   const answerBtnClicked = (e) => {
+    setbtnDisabled(true)
     let resultsScore;
     e.target.innerText === question[1].answer
       ? (resultsScore = 20)
@@ -12,18 +15,18 @@ const Question2 = ({ question }) => {
     <div className={'questionDIV'}>
       <h1 className={'title'}>{question[1].title}</h1>
       <div className='topQuestions'>
-        <button onClick={answerBtnClicked} className={'button'}>
+        <button disabled={btnDisabled} onClick={answerBtnClicked} className={'button'}>
           <h5>{question[1].choices[0]}</h5>
         </button>
-        <button onClick={answerBtnClicked} className={'button'}>
+        <button disabled={btnDisabled} onClick={answerBtnClicked} className={'button'}>
           <h5>{question[1].choices[1]}</h5>
         </button>
       </div>
       <div className='bottomQuestions'>
-        <button onClick={answerBtnClicked} className={'button'}>
+        <button disabled={btnDisabled} onClick={answerBtnClicked} className={'button'}>
           <h5>{question[1].choices[2]}</h5>
         </button>
-        <button onClick={answerBtnClicked} className={'button'}>
+        <button disabled={btnDisabled} onClick={answerBtnClicked} className={'button'}>
           <h5>{question[1].choices[3]}</h5>
         </button>
       </div>
--- client/src/components/Pages/Quiz/Question3/Question3.js
+++ client/src/components/Pages/Quiz/Question3/Question3.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Question3 = ({ question }) => {
+  const [btnDisabled, setbtnDisabled] = useState(false);
   const answerBtnClicked = (e) => {
+    setbtnDisabled(true);
     let resultsScore;
     e.target.innerText === question[2].answer
       ? (resultsScore = 20)
@@ -12,18 +14,34 @@ const Question3 = ({ question }) => {
     <div className={'questionDIV'}>
       <h1 className={'title'}>{question[2].title}</h1>
       <div className='topQuestions'>
-        <button onClick={answerBtnClicked} className={'button'}>
+        <button
+          disabled={btnDisabled}
+          onClick={answerBtnClicked}
+          className={'button'}
+        >
           <h5>{question[2].choices[0]}</h5>
         </button>
-        <button onClick={answerBtnClicked} className={'button'}>
+        <button
+          disabled={btnDisabled}
+          onClick={answerBtnClicked}
+          className={'button'}
+        >
           <h5>{question[2].choices[1]}</h5>
         </button>
       </div>
       <div className='bottomQuestions'>
-        <button onClick={answerBtnClicked} className={'button'}>
+        <button
+          disabled={btnDisabled}
+          onClick={answerBtnClicked}
+          className={'button'}
+        >
           <h5>{question[2].choices[2]}</h5>
         </button>
-        <button onClick={answerBtnClicked} className={'button'}>
+        <button
+          disabled={btnDisabled}
+          onClick={answerBtnClicked}
+          className={'button'}
+        >
           <h5>{question[2].choices[3]}</h5>
         </button>
       </div>
