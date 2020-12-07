d76015a8db70096653ea7f7a1beb2654f8669d5f
BrennanPredmore <brennnann@gmail.com>
2020-12-07 15:46:05 -0600

    all buttons onClick event contact has been made

--- client/src/components/Pages/Quiz/Question4/Question4.js
+++ client/src/components/Pages/Quiz/Question4/Question4.js
@@ -1,22 +1,25 @@
 import React from 'react';
 
 const Question4 = ({ question }) => {
+  const answerBtnClicked = (e) => {
+    console.log(e.target.innerText)
+  }
   return (
     <div className={'questionDIV'}>
       <h1 className={'title'}>{question[3].title}</h1>
       <div className='topQuestions'>
-        <button className={'button'}>
+        <button onClick={answerBtnClicked} className={'button'}>
           <h5>{question[3].choices[0]}</h5>
         </button>
-        <button className={'button'}>
+        <button onClick={answerBtnClicked} className={'button'}>
           <h5>{question[3].choices[1]}</h5>
         </button>
       </div>
       <div className='bottomQuestions'>
-        <button className={'button'}>
+        <button onClick={answerBtnClicked} className={'button'}>
           <h5>{question[3].choices[2]}</h5>
         </button>
-        <button className={'button'}>
+        <button onClick={answerBtnClicked} className={'button'}>
           <h5>{question[3].choices[3]}</h5>
         </button>
       </div>
