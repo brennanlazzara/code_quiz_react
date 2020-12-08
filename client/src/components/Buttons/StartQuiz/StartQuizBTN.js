c0a42899511ff2cbcc6f7de46d5ff8c84baab27d
BrennanPredmore <brennnann@gmail.com>
2020-12-08 14:48:17 -0600

    brenn fix merge error

--- client/src/components/Buttons/StartQuiz/StartQuizBTN.js
+++ client/src/components/Buttons/StartQuiz/StartQuizBTN.js
@@ -1,18 +1,7 @@
 import React from 'react';
 import './style.css';
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD:client/src/components/Buttons/StartLesson/StartLessonBTN.js
 const StartLessonBTN = () => {
-  return (
-    <div className='container'>
-      <div className='center'>
-        <button className='button'>
-          <a href='/Lesson'>
-=======
->>>>>>> 4c8b013136e379237b235d212074ca87e2f235bc
-const StartMenu = () => {
   const removeLocalStorage = () => {
     localStorage.removeItem('results');
     localStorage.removeItem('name');
@@ -22,14 +11,8 @@ const StartMenu = () => {
       <div className='center'>
         <button onClick={removeLocalStorage} className='button'>
           <a href='/Quiz'>
-<<<<<<< HEAD
             {' '}
             <h3> Start Quiz</h3>
-=======
->>>>>>> 4c8b013136e379237b235d212074ca87e2f235bc:client/src/components/Buttons/StartQuiz/StartQuizBTN.js
-            {' '}
-            <h3> Start Lesson</h3>
->>>>>>> 4c8b013136e379237b235d212074ca87e2f235bc
           </a>
         </button>
       </div>
@@ -37,8 +20,6 @@ const StartMenu = () => {
   );
 };
 
-<<<<<<< HEAD
-export default StartMenu;
-=======
+
 export default StartLessonBTN;
->>>>>>> 4c8b013136e379237b235d212074ca87e2f235bc
+
