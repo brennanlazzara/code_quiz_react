cc80ae812f85159bdad682426d660854307b8890
BrennanPredmore <brennnann@gmail.com>
2020-12-08 11:22:31 -0600

    onClick for viewResultsBTN

--- client/src/components/Buttons/ViewResults/ViewResultsBTN.js
+++ client/src/components/Buttons/ViewResults/ViewResultsBTN.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './style.css';
 
 const ViewResultsBTN = () => {
+const viewResults = () => {
+  alert('HELLO')
+}
   return (
     <div className='container'>
       <div className='center'>
-        <button className='button'>
+        <button onClick={viewResults} className='button'>
           <a href='/Results'>
             {' '}
             <h3> View Your Results</h3>
