768de14b6994d394c7ef1d583635a650e7c7dffc
shawn220a <shawn.42496@gmail.com>
2020-12-09 15:05:37 -0600

    Sent axios call to mongoDB to create the results

--- controller/results.controller.js
+++ controller/results.controller.js
@@ -3,6 +3,8 @@ const resultsModel = require('../models/results.model');
 exports.createResults = async (req, res, next) => {
   try {
     console.log(req.body);
+    const newResult = await resultsModel.create(req.body);
+    res.status(201).json(newResult);
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
