d6e31b02fcbf84dd949adf16001e172188b03f01
shawn220a <shawn.42496@gmail.com>
2020-12-09 15:26:35 -0600

    Made axios call to retrieve resutls

--- client/src/components/HighscoresTable/HighscoresTable.js
+++ client/src/components/HighscoresTable/HighscoresTable.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import { Table } from 'reactstrap';
 
 import './styles.css';
@@ -10,19 +11,37 @@ class HighscoresTable extends Component {
   }
 
   getResults() {
-    let name = JSON.parse(localStorage.getItem('name'));
-    let result = JSON.parse(localStorage.getItem('results'));
-    this.setState({
-      name: name,
-      result: result,
+    axios.get('/api/results').then((res) => {
+      console.log(res.data);
+      this.setState([res.data]);
     });
   }
 
   render() {
-    if (!this.state) {
+    if (this.state) {
       return (
         <div>
+          <h1>Your HighScores</h1>
           <div>{console.log(this.state)}</div>
+          {/* <Table>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Results</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.map((e) => {
+                return (
+                  <tr key={e._id}>
+                    <td>{e.name}</td>
+                    <td>{e.results}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table> */}
         </div>
       );
     } else {
