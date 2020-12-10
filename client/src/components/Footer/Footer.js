1a5b790cac6588e6b2d8d01a4de6bb3018ef478d
BrennanPredmore <brennnann@gmail.com>
2020-12-10 13:16:31 -0600

    footer stlying refactor // media quieries home page INIT!

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,52 +5,23 @@ import { faGithubAlt, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
-    <div className='footer-dark' style={{ padding: 12 }}>
-      <footer>
-        <div className='container'>
-          <div className='row' style={{ height: 64 }}>
-            <div
-              className='col-sm-6 col-md-3 item'
-              style={{ height: 0, minHeight: 4, margin: 'auto' }}
-            >
-              <img
-                src='assets/img/dyhIcon.png'
-                alt={'footer'}
-                width='128px'
-                height='128px'
-                style={{ width: 75, height: 75 }}
+    <>
+      <div className='footer-dark container-fluid' style={{ padding: 12 }}>
+        <div className='row' style={{ height: 64 }}>
+          <div className='col item social'>
+            <a href='https://github.com/BrennanPredmore'>
+              <FontAwesomeIcon
+                style={{ color: 'red', background: '333' }}
+                icon={faGithubAlt}
               />
-            </div>
-            <div
-              className='col item social'
-              style={{
-                height: 64,
-                marginTop: 150,
-                display: 'flex',
-                justifyContent: 'space-between',
-              }}
-            >
-              <a href='https://github.com/BrennanPredmore'>
-                <FontAwesomeIcon
-                  style={{ color: 'red', background: '333' }}
-                  icon={faGithubAlt}
-                />
-              </a>
-              <a href='https://www.linkedin.com/in/brennan-predmore-132306195/'>
-                <FontAwesomeIcon style={{ color: 'red' }} icon={faLinkedin} />
-              </a>
-            </div>
-
-            <div
-              className='col-md-6 item text'
-              style={{ maxWidth: 266, height: 64, minHeight: 64 }}
-            >
-              <h3 style={{ marginBottom: 0 }}>Do Your Homework© 2020</h3>
-            </div>
+            </a>
+            <a href='https://www.linkedin.com/in/brennan-predmore-132306195/'>
+              <FontAwesomeIcon style={{ color: 'red' }} icon={faLinkedin} />
+            </a>
           </div>
         </div>
-      </footer>
-    </div>
+      </div>
+    </>
   );
 };
 
