099eb613c89c47b1a5502a19c18d6dad5e09c59c
BrennanPredmore <brennnann@gmail.com>
2020-12-10 12:20:48 -0600

    refactor App.js // init App.css

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,25 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from '../src/components/Pages/Home/Home';
 import Lesson from '../src/components/Pages/Lesson/Lesson';
 import Quiz from '../src/components/Pages/Quiz/Quiz';
-// import Question2 from '../src/components/Pages/Quiz/Question1/Question2';
-// import Question3 from '../src/components/Pages/Quiz/Question1/Question3';
-// import Question4 from '../src/components/Pages/Quiz/Question1/Question4';
-// import Question5 from '../src/components/Pages/Quiz/Question1/Question5';
 import Results from '../src/components/Pages/Results/Results';
 import ViewHighscores from './components/Pages/ViewHighscores/ViewHighscores';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer'
+import './App.css';
 
 function App() {
   return (
     <Router>
       <div className='App'>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/Lesson' component={Lesson} />
-        <Route exact path='/Quiz' component={Quiz} />
-        {/* <Route exact path='/Question2' component={Question2} />
-      <Route exact path='/Question3' component={Question3} />
-      <Route exact path='/Question4' component={Question4} />
-      <Route exact path='/Question5' component={Question5} /> */}
-        <Route exact path='/Results' component={Results} />
-        <Route exact path='/ViewHighscores' component={ViewHighscores} />
+        <Header />
+        <div className='container'>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/Lesson' component={Lesson} />
+          <Route exact path='/Quiz' component={Quiz} />
+          <Route exact path='/Results' component={Results} />
+          <Route exact path='/ViewHighscores' component={ViewHighscores} />
+        </div>
+        <Footer />
       </div>
     </Router>
   );
