e4af881735fe799c8d95553b71eddacc72034fb3
BrennnanPredmore <brennnann@gmail.com>
2021-02-02 16:44:09 -0600

    preparing for heroku build

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const db = require('./mongoDB/connection');
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -8,7 +9,15 @@ app.use(express.static('./public'));
 
 app.use('/api', require('./routes/api-routes'));
 
+// SERVE STATIC ASSETS IF IN PRODUCTION
+if (process.env.NODE_ENV) {
+  // SET STATIC FOLDER
+  app.use(express.static('client/build'));
 
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 db.connect();
 
